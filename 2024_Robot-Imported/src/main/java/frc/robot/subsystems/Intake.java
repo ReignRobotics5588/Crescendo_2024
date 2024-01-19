c885975e0f054892874834abd57e36cbae65d6c7
G-Spadez <Grosen25@holynames-sea.org>
2024-01-19 15:40:45 -0800

    Added sensor code

--- 2024_Robot-Imported/src/main/java/frc/robot/subsystems/Intake.java
+++ 2024_Robot-Imported/src/main/java/frc/robot/subsystems/Intake.java
@@ -8,12 +8,19 @@ import com.revrobotics.*;
 public class Intake extends SubsystemBase {
     private final CANSparkMax m_intakeMotor = new CANSparkMax(IntakeConstants.kMotorPort,MotorType.kBrushless);
     private final DigitalInput m_intakeSensor = new DigitalInput(IntakeConstants.sensorPort);
-    //To Do: add sensor - find out what hardware is 
+    private final DigitalInput m_intakeLight = new DigitalInput(IntakeConstants.lightPort);
     public Intake () {
         super();
     } 
     public void run(double speed) {
         m_intakeMotor.set(speed);
+        if (sense()) {
+            m_intakeMotor.set(speed);
+        } else {
+            m_intakeMotor.set(0);
+        }
     } 
-    
+    public boolean sense() {
+        return m_intakeSensor.get();
+    }
 }
