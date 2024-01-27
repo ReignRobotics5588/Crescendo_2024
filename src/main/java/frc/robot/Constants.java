6b7c5cb03b281d56df0637c6115fb29d8ae34c52
sf3990 <SFong25@holynames-sea.org>
2024-01-26 16:26:48 -0800

    a

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,7 @@ package frc.robot;
 public final class Constants {
   public static final class DriveConstants {
     public static final int kFrontLeftMotorPort = 1;
+    public static final int kClimberPort = 5; 
     public static final int kRearLeftMotorPort = 3;
 
     public static final int kFrontRightMotorPort = 2;
