42ea472d3925d031036440556fd7dcce8e8d3eea
G-Spadez <Grosen25@holynames-sea.org>
2024-01-30 17:17:09 -0800

    added 2nd intake motor

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,21 +5,29 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 import com.revrobotics.*;
 public class Intake extends SubsystemBase {
-    private final CANSparkMax m_intakeMotor = new CANSparkMax(IntakeConstants.kMotorPort,MotorType.kBrushless);
+    private final CANSparkMax m_leftIntakeMotor = new CANSparkMax(IntakeConstants.kMotorPort,MotorType.kBrushless);
+    private final CANSparkMax m_rightIntakeMotor = new CANSparkMax(IntakeConstants.kMotorPort, MotorType.kBrushless);
     private final DigitalInput m_intakeSensor = new DigitalInput(IntakeConstants.sensorPort);
     private final DigitalInput m_intakeLight = new DigitalInput(IntakeConstants.lightPort);
     public Intake () {
         super();
     } 
     public void run(double speed) {
-        m_intakeMotor.set(speed);
+        runMotors(speed);
         if (sense()) {
-            m_intakeMotor.set(speed);
+           runMotors(speed);
         } else {
-            m_intakeMotor.set(0);
+            runMotors(0);
         }
     } 
     public boolean sense() {
         return m_intakeSensor.get();
+    
+    }
+
+    public void runMotors(double speed) {
+        m_leftIntakeMotor.set(speed);
+        m_rightIntakeMotor.set(speed);
+        
     }
 }
