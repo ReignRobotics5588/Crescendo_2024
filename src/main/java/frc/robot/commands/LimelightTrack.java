3282008c151b5853ef826fbf77e7a54a28741b0a
Zachary Holly <zholly18@outlook.com>
2024-02-14 12:32:58 -0800

    Update limelight references

--- src/main/java/frc/robot/commands/LimelightTrack.java
+++ src/main/java/frc/robot/commands/LimelightTrack.java
@@ -1,20 +1,10 @@
 package frc.robot.commands;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import frc.robot.subsystems.LimelightSubsystem;
-import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.*;
 import frc.robot.Constants;
-import frc.robot.RobotContainer;
 
-public class LimelightTrack extends CommandBase{
+public class LimelightTrack extends Command{
     private final LimelightSubsystem m_LimelightSubsystem;
     private final Drivetrain m_DriveSubsystem;
 
@@ -31,12 +21,12 @@ public class LimelightTrack extends CommandBase{
       // Called every time the scheduler runs while the command is scheduled.
       @Override
       public void execute() {
-          if(!RobotContainer.m_LimelightSubsystem.hasTarget()){
+          if(!m_LimelightSubsystem.hasTarget()){
             m_DriveSubsystem.tankDrive(0,0);//uhhhhh ask zach
           }
           else{
-            System.out.print(RobotContainer.m_LimelightSubsystem.getX());
-            double speed = RobotContainer.m_LimelightSubsystem.getX()*Constants.K_TURN;
+            System.out.print(m_LimelightSubsystem.getX());
+            double speed = m_LimelightSubsystem.getX()*Constants.K_TURN;
             speed = MathUtil.clamp(speed, -1 * Constants.MAX_SPEED, Constants.MAX_SPEED);//speed, low, high
             m_DriveSubsystem.tankDrive(speed, -speed);//UHHHHHH ASK ZACH
 
@@ -51,7 +41,7 @@ public class LimelightTrack extends CommandBase{
       // Returns true when the command should end.
       @Override
       public boolean isFinished() {
-        return (RobotContainer.m_LimelightSubsystem.getX() <= 1.5 && RobotContainer.m_LimelightSubsystem.getX() >=-1.5);
+        return (m_LimelightSubsystem.getX() <= 1.5 && m_LimelightSubsystem.getX() >=-1.5);
       }//between -1.5->1.5
 
 
