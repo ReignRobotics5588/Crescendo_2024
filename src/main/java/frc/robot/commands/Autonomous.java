b32ea47d8b2a92fc7035ef48a1bdc325b7f66d07
GoldTails1 <117782798+GoldTails1@users.noreply.github.com>
2024-02-29 16:04:47 -0800

    deleted k

---
+++ src/main/java/frc/robot/commands/Autonomous.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.Constants.AutoConstants;
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+/** The main autonomous command to pickup and deliver the soda to the box. */
+public class Autonomous extends SequentialCommandGroup {
+  // changed DriveSubsystem to (Drivetrain drive)
+  public Autonomous(Drivetrain drive){
+    addCommands(null);
+
+  }
+}
