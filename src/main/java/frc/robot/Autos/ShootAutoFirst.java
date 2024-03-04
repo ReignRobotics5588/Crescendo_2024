9018dd951aebe5305bee8734970cdda83e791cde
zholly15 <zholly18@outlook.com>
2024-03-04 11:53:13 -0800

    Update Gradle and Finish Shoot Refactor

--- src/main/java/frc/robot/Autos/ShootAutoFirst.java
+++ src/main/java/frc/robot/Autos/ShootAutoFirst.java
@@ -2,6 +2,7 @@ package frc.robot.Autos;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.DriveCommand;
+import frc.robot.commands.Shoot;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Intake;
--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -1,13 +1,9 @@
-package frc.robot.Autos;
+package frc.robot.commands;
 import frc.robot.subsystems.*;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-/**
- * A simple command that grabs a hatch with the {@link HatchSubsystem}. Written explicitly for
- * pedagogical purposes. Actual code should inline a command this simple with {@link
- * edu.wpi.first.wpilibj2.command.InstantCommand}.
- */
+
 public class Shoot extends Command {
   // The subsystem the command runs on
   private final Shooter m_shoot;
